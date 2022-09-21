89072a92c5e478cf2994436bea6cc98a79a0d7c5
RobinVanYang <thehappyone@163.com>
2022-09-22 05:09:30 +0800

    update

--- common_cmd.py
+++ common_cmd.py
@@ -7,7 +7,7 @@ from argos import Argos
 
 
 def run_cmd(cmd) -> Result[str, str]:
-    Argos.i(cmd)
+    Argos.d(cmd)
     # p_open = subprocess.Popen(cmd, text=True,encoding="UTF-8", shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
     # stdout, stderr = p_open.communicate("Y\n")
     # if stdout is None:
