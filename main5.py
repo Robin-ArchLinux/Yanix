83b378795ec15555d102908c148712ec9a062d29
RobinVanYang <thehappyone@163.com>
2022-09-24 06:02:12 +0800

    update

--- main5.py
+++ main5.py
@@ -1,5 +1,5 @@
 import subprocess
 
 cmd = "sudo pacman -Syu --needed code"
-res = subprocess.run(cmd, shell=True, text=True, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+res = subprocess.run(cmd, shell=True, text=True, stdin=None, stdout=None, stderr=subprocess.PIPE,
                      encoding="utf8")
