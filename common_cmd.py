ce66a4e183703d59d7032609815dbdbe4b4a642d
RobinVanYang <thehappyone@163.com>
2022-09-24 06:12:29 +0800

    update

--- common_cmd.py
+++ common_cmd.py
@@ -1,5 +1,10 @@
+import os
 import subprocess
 import sys
+import select
+import termios
+import tty
+import pty
 from typing import List
 from result import Result, Ok, Err
 from constant import ROOT_DIR
@@ -24,12 +29,42 @@ def run_cmd(cmd, pipe=False, cwd=None) -> Result[str, str]:
         else:
             return Err(res.stderr.strip())
     else:
-        res = subprocess.run(cmd, shell=True, text=True, stdin=None, stdout=None, stderr=subprocess.PIPE,
-                             encoding="utf8", cwd=cwd)
+        res = subprocess.run(cmd, shell=True, text=True, stdin=None, stdout=None, stderr=None, cwd=cwd)
         if res.returncode == 0:
             return Ok()
         else:
             return Err(res.stderr.strip())
+
+
+def run_cmd_with_interactive(command, cwd=None):
+    # save original tty setting then set it to raw mode
+    old_tty = termios.tcgetattr(sys.stdin)
+    tty.setraw(sys.stdin.fileno())
+
+    # open pseudo-terminal to interact with subprocess
+    master_fd, slave_fd = pty.openpty()
+
+    try:
+        # use os.setsid() make it run in a new process group, or bash job control will not be enabled
+        p = subprocess.Popen(command,
+                             preexec_fn=os.setsid,
+                             stdin=slave_fd,
+                             stdout=slave_fd,
+                             stderr=slave_fd,
+                             universal_newlines=True, cwd=cwd)
+
+        while p.poll() is None:
+            r, w, e = select.select([sys.stdin, master_fd], [], [])
+            if sys.stdin in r:
+                d = os.read(sys.stdin.fileno(), 10240)
+                os.write(master_fd, d)
+            elif master_fd in r:
+                o = os.read(master_fd, 10240)
+                if o:
+                    os.write(sys.stdout.fileno(), o)
+    finally:
+        # restore tty settings back
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_tty)
 
 
 def pac_install(package):
@@ -89,7 +124,7 @@ def install_paru():
                 case Ok(_):
                     Argos.s(f"install paru success")
                 case Err(e):
-                    Argos.e(f"install paru failed")
+                    Argos.e(f"install paru failed, err: {e}")
 
 
 def aur_install(package):
